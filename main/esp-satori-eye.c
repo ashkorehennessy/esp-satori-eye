bfbec9f4609a9a3667b191c853d53d29c31ab61b
ashkorehennessy <ashkorehennessy@gmail.com>
2026-02-09 08:40:05 +0800

    特性: 集成OV2640驱动并实现基础采集功能
    1. 依赖管理：
       - 新增 `idf_component.yml`，引入官方 `espressif/esp32-camera` 组件。
       - 提交 `dependencies.lock` 以锁定依赖版本。
    
    2. SDK配置优化：
       - 启用 OV2640 驱动支持，禁用其他未使用的摄像头型号（如 OV7670, OV3660 等）以减小固件体积。
       - 开启 `LCD_CAM_ISR_IRAM_SAFE`，将中断服务放入 IRAM 以提升图像采集稳定性。
       - 配置 SCCB (I2C) 端口及频率。
    
    3. 驱动实现：
       - 初始化摄像头配置结构体（配置具体引脚映射）。
       - 实现主循环：每秒采集一帧图像并打印数据长度，用于验证硬件连接。

--- main/esp-satori-eye.c
+++ main/esp-satori-eye.c
@@ -1,14 +1,58 @@
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include "esp_chip_info.h"
 #include "esp_log.h"
-#include "esp_heap_caps.h"
-#include "esp_system.h"
-
+#include "esp_camera.h"
+static camera_config_t camera_config = {
+    .pin_pwdn  = GPIO_NUM_NC,
+    .pin_reset = GPIO_NUM_NC,
+    .pin_xclk = GPIO_NUM_15,
+    .pin_sccb_sda = GPIO_NUM_4,
+    .pin_sccb_scl = GPIO_NUM_5,
+    .pin_d7 = GPIO_NUM_16,
+    .pin_d6 = GPIO_NUM_17,
+    .pin_d5 = GPIO_NUM_18,
+    .pin_d4 = GPIO_NUM_12,
+    .pin_d3 = GPIO_NUM_10,
+    .pin_d2 = GPIO_NUM_8,
+    .pin_d1 = GPIO_NUM_9,
+    .pin_d0 = GPIO_NUM_11,
+    .pin_vsync = GPIO_NUM_6,
+    .pin_href = GPIO_NUM_7,
+    .pin_pclk = GPIO_NUM_13,
+    .sccb_i2c_port = 1,
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+    .pixel_format = PIXFORMAT_JPEG,
+    .frame_size = FRAMESIZE_QVGA,
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .jpeg_quality = 12,
+    .fb_count = 2,
+    .grab_mode = CAMERA_GRAB_LATEST
+};
 void app_main(void)
 {
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE("Camera", "Camera Init Failed");
+        ESP_LOGE("System", "System Halted");
+        return;
+    }
+    ESP_LOGI("Camera", "Camera Init Success");
+    while (1) {
+        camera_fb_t *pic = esp_camera_fb_get();
+        if (pic) {
+            ESP_LOGI("Camera", "Picture taken! Size: %zu bytes, Width: %d, Height: %d",
+                     pic->len, pic->width, pic->height);
 
-    size_t free_psram = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
-    printf("Total Free PSRAM: %d Bytes (%.2f MB)\n", free_psram, free_psram / 1024.0 / 1024.0);
+            esp_camera_fb_return(pic);
+        } else {
+            ESP_LOGE("Camera", "Failed to capture frame!");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
+
